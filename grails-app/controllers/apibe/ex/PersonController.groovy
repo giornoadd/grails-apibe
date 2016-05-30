a79f160d2b5baa159a6148318b1b9dcf50bef8eb
adthasis <adthasis@ais.co.th>
2016-05-30 16:05:45 +0700

    Add secure to server

--- grails-app/controllers/apibe/ex/PersonController.groovy
+++ grails-app/controllers/apibe/ex/PersonController.groovy
@@ -1,9 +1,9 @@
 package apibe.ex
 
-// import grails.plugin.springsecurity.annotation.Secured
+import grails.plugin.springsecurity.annotation.Secured
 import grails.rest.RestfulController
 
-// @Secured(['ROLE_USER'])
+@Secured(['ROLE_USER'])
 class PersonController extends RestfulController {
 
     static responseFormats = ['json']
